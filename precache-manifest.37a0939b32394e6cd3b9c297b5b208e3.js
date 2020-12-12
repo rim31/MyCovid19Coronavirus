fa9133acedb4468978824517f489078371b933a4
toto42 <toto42@yopmail.com>
2020-12-12 15:49:03 +0200

    url router fixing

---
+++ precache-manifest.37a0939b32394e6cd3b9c297b5b208e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "382a71d35666315d9de37034e0344300",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "b12cd7bacb858b096e58",
+    "url": "/covid19/static/css/main.36c13af1.chunk.css"
+  },
+  {
+    "revision": "34646036f6b8c18d8d58",
+    "url": "/covid19/static/js/2.1c173d82.chunk.js"
+  },
+  {
+    "revision": "9032d824e79934af1e549b58aad0c579",
+    "url": "/covid19/static/js/2.1c173d82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b12cd7bacb858b096e58",
+    "url": "/covid19/static/js/main.707e887f.chunk.js"
+  },
+  {
+    "revision": "6ce13c9e59509319f789",
+    "url": "/covid19/static/js/runtime-main.a76cae78.js"
+  }
+]);
