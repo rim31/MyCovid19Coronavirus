43175ad37d27cfc773e2d4b98b35e6901a96e1aa
Rim S <rims@MacBook-Pro-de-Rim.local>
2021-04-06 10:52:33 +0200

    update API for France, US need to be fix

---
+++ precache-manifest.2d89c78bca2e2f84a0f895166f419c7b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5a483913d9c885517fcc753bf5180fda",
+    "url": "/covid19/index.html"
+  },
+  {
+    "revision": "c8d3eaa256cc106478d4",
+    "url": "/covid19/static/css/main.36c13af1.chunk.css"
+  },
+  {
+    "revision": "36c4c3d003dc179b0ccd",
+    "url": "/covid19/static/js/2.31dc81b1.chunk.js"
+  },
+  {
+    "revision": "9032d824e79934af1e549b58aad0c579",
+    "url": "/covid19/static/js/2.31dc81b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d3eaa256cc106478d4",
+    "url": "/covid19/static/js/main.7c53bfc8.chunk.js"
+  },
+  {
+    "revision": "6ce13c9e59509319f789",
+    "url": "/covid19/static/js/runtime-main.a76cae78.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/covid19/precache-manifest.a7c7ea13f2100a3112a55c2790d25db6.js"
+  "/covid19/precache-manifest.2d89c78bca2e2f84a0f895166f419c7b.js"
 );
 
 self.addEventListener('message', (event) => {
